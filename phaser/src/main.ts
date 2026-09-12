29b4c503006678e6b753b7d2ed0156d7b7875ac0
xiaoQDR <347926431@qq.com>
2026-09-12 14:51:30 +0800

    feat: enable explorer job and slower worker movement

--- phaser/src/main.ts
+++ phaser/src/main.ts
@@ -10,10 +10,12 @@ import { installOriginalBalancePatch } from './ui/originalBalancePatch';
 import { installDynamicBuildCostPatch } from './ui/dynamicBuildCostPatch';
 import { installTestTimeScalePatch } from './ui/testTimeScalePatch';
 import { installHunterWorkPatch } from './ui/hunterWorkPatch';
+import { installExplorerWorkPatch } from './ui/explorerWorkPatch';
 import { installDebugResourcePatch } from './ui/debugResourcePatch';
 import { installTradingPostPatch } from './ui/tradingPostPatch';
 import { installBuildingWorkerPatch } from './ui/buildingWorkerPatch';
 import { installWorkerIdentityPatch } from './ui/workerIdentityPatch';
+import { installWorkerMovementSpeedPatch } from './ui/workerMovementSpeedPatch';
 
 installWorldExpansionPatch();
 installJobAppearancePatch();
@@ -24,10 +26,12 @@ installOriginalBalancePatch();
 installDynamicBuildCostPatch();
 installTestTimeScalePatch();
 installHunterWorkPatch();
+installExplorerWorkPatch();
 installDebugResourcePatch();
 installTradingPostPatch();
 installBuildingWorkerPatch();
 installWorkerIdentityPatch();
+installWorkerMovementSpeedPatch();
 
 new Phaser.Game({
   type: Phaser.AUTO,
