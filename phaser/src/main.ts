0ee269469e7c2ef9a1a05301026e8e73616ee5ac
xiaoQDR <347926431@qq.com>
2026-09-12 14:26:29 +0800

    feat: enable expanded world and profession outfits

--- phaser/src/main.ts
+++ phaser/src/main.ts
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
 import './style.css';
 import { BuildScene } from './scenes/BuildScene';
+import { installWorldExpansionPatch } from './ui/worldExpansionPatch';
+import { installJobAppearancePatch } from './ui/jobAppearancePatch';
 import { installJobPanelPatch } from './ui/jobPanelPatch';
 import { installTrapSystemPatch } from './ui/trapSystemPatch';
 import { installEventSystemPatch } from './ui/eventSystemPatch';
@@ -13,6 +15,8 @@ import { installTradingPostPatch } from './ui/tradingPostPatch';
 import { installBuildingWorkerPatch } from './ui/buildingWorkerPatch';
 import { installWorkerIdentityPatch } from './ui/workerIdentityPatch';
 
+installWorldExpansionPatch();
+installJobAppearancePatch();
 installJobPanelPatch();
 installTrapSystemPatch();
 installEventSystemPatch();
